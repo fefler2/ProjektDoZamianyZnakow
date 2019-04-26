894be27f826f1565aed16449dba85c095d604ebb
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-26 17:54:39 +0200

    koncowa wersja 1 - zrobienie inputow

--- src/main/java/ProjektZamianaZnakow/ZamianaZnakow.java
+++ src/main/java/ProjektZamianaZnakow/ZamianaZnakow.java
@@ -11,7 +11,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
 
 public class ZamianaZnakow extends Application {
     @Override
@@ -55,21 +57,27 @@ public class ZamianaZnakow extends Application {
 //        rootNode.add(result, 1, 3);
 
         executiveButton.setOnAction(e -> {
-            //text file, should be opening in default text editor
-            File file = new File("/Users/pankaj/source.txt");
+            try {
+                //text file, should be opening in default text editor
+                File file = new File("/Users/pankaj/source.txt");
 
-            //first check if Desktop is supported by Platform or not
-            if(!Desktop.isDesktopSupported()){
-                System.out.println("Desktop is not supported");
-                return;
+                //first check if Desktop is supported by Platform or not
+                if(!Desktop.isDesktopSupported()){
+                    System.out.println("Desktop is not supported");
+                    return;
+                }
+
+                Desktop desktop = Desktop.getDesktop();
+                if(file.exists()) desktop.open(file);
+
+                //let's try to open PDF file
+                file = new File("/Users/pankaj/java.pdf");
+                if(file.exists()) desktop.open(file);
+            }
+            catch (IOException e1){
+                e1.printStackTrace();
             }
 
-            Desktop desktop = Desktop.getDesktop();
-            if(file.exists()) desktop.open(file);
-
-            //let's try to open PDF file
-            file = new File("/Users/pankaj/java.pdf");
-            if(file.exists()) desktop.open(file);
         });
 
 //        aButton.setOnAction(e -> {

160de4d8f85352b40b1e383ce0f1552500160d94
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-27 08:53:24 +0200

    dziala cos2 - nowa wersja8

--- src/main/java/ProjektZamianaZnakow/ZamianaZnakow.java
+++ src/main/java/ProjektZamianaZnakow/ZamianaZnakow.java
@@ -23,6 +23,8 @@ import java.io.*;
 import java.util.HashSet;
 
 public class ZamianaZnakow extends Application {
+
+
     @Override
     public void start(Stage myStage) {
 
@@ -126,6 +128,7 @@ public class ZamianaZnakow extends Application {
                         //
 
 
+
                         /////
                         /////
                         File root = new File("C:\\Users\\Maciej\\Desktop\\Java Projects3\\zamianaZnakow\\");
@@ -182,6 +185,57 @@ public class ZamianaZnakow extends Application {
 //                        }
 //
 //                    }
+
+                    ////
+                    ////
+                    ////
+                    // PrintWriter object for output.txt
+                    PrintWriter pw = new PrintWriter(filePath);
+
+                    // BufferedReader object for delete.txt
+                    BufferedReader br2 = new BufferedReader(new FileReader(filePath));
+
+                    String line2 = br2.readLine();
+
+                    // hashset for storing lines of delete.txt
+                    HashSet<String> hs = new HashSet<String>();
+
+                    // loop for each line of delete.txt
+                    while(line2 != null)
+                    {
+                        hs.add(line2);
+                        line2 = br2.readLine();
+                    }
+
+                    // BufferedReader object for input.txt
+                    BufferedReader br1 = new BufferedReader(new FileReader("input.txt"));
+
+                    String line1 = br1.readLine();
+
+                    // loop for each line of input.txt
+                    while(line1 != null)
+                    {
+                        // if line is not present in delete.txt
+                        // write it to output.txt
+                        if(!hs.contains(line1))
+                            pw.println(line1);
+
+                        line1 = br1.readLine();
+                    }
+
+                    pw.flush();
+
+                    // closing resources
+                    br1.close();
+                    br2.close();
+                    pw.close();
+
+                    System.out.println("File operation performed successfully");
+
+
+                ////
+                    ////
+                    ////
 
                 } catch (Exception e3) {
                     e3.printStackTrace();
